292553679cea8c333324da8547d2bc586b3f8fec
Rheeders <sparky79131>
2019-04-03 16:44:03 +0200

    FacebookGoogleLogin

--- app/src/main/java/wethinkcode/co/za/matcha/Account.java
+++ app/src/main/java/wethinkcode/co/za/matcha/Account.java
@@ -1,11 +1,19 @@
 package wethinkcode.co.za.matcha;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class Account extends AppCompatActivity {
 
@@ -13,15 +21,15 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button registerBttn = findViewById(R.id.logoutBttn);
+        registerBttn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                LoginManager.getInstance().logOut();
+                Intent gotoRegister = new Intent(getApplicationContext(), Register.class);
+                startActivity(gotoRegister);
             }
         });
     }
--- app/src/main/java/wethinkcode/co/za/matcha/Register.java
+++ app/src/main/java/wethinkcode/co/za/matcha/Register.java
@@ -3,6 +3,7 @@ package wethinkcode.co.za.matcha;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,13 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,7 +43,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Pattern;
 
-public class Register extends AppCompatActivity
+
+public class  Register extends AppCompatActivity
 {
     private static final Pattern PASSWORD_LENGTH =
         Pattern.compile("^.{8,}$");
@@ -120,6 +129,9 @@ public class Register extends AppCompatActivity
     private CallbackManager mCallbackManager;
     private LoginButton loginButton;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+
 
 
     @Override
@@ -128,6 +140,12 @@ public class Register extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
         DatabaseReference users = database.child("users");
@@ -137,9 +155,12 @@ public class Register extends AppCompatActivity
         loginButton.setReadPermissions("email", "public_profile", "user_gender", "user_birthday", "user_location" , "user_photos");
 
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+
             @Override
             public void onSuccess(LoginResult loginResult) {
                 handleFacebookAccessToken(loginResult.getAccessToken());
+                loginButton.setEnabled(false);
+
             }
 
             @Override
@@ -153,9 +174,21 @@ public class Register extends AppCompatActivity
             }
         });
 
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-
 
         Button registerSubmit = findViewById(R.id.registerSubmit);
         registerSubmit.setOnClickListener(new View.OnClickListener()
@@ -180,16 +213,39 @@ public class Register extends AppCompatActivity
 
                     user = new User(Username, Email, FirstName, Surname, PW, "", "", "");
                     users.push().setValue(user);
+
+                    Intent gotoAccount = new Intent(getApplicationContext(), Account.class);
+                    startActivity(gotoAccount);
                 }
             }
         });
     }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(Register.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                // ...
+            }
+/*            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);*/
+        }
+        else {
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -200,6 +256,30 @@ public class Register extends AppCompatActivity
         if (currentUser != null) {
             updateUI(currentUser);
         }
+
+/*        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            updateGUI(account);
+        }*/
+
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateGUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void updateUI(FirebaseUser currentUser){
@@ -207,6 +287,36 @@ public class Register extends AppCompatActivity
         Intent gotoAccount = new Intent(getApplicationContext(), Account.class);
         startActivity(gotoAccount);
 
+    }
+
+    private void updateGUI(GoogleSignInAccount currentUser){
+
+        Intent gotoAccount = new Intent(getApplicationContext(), Account.class);
+        startActivity(gotoAccount);
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
@@ -220,10 +330,14 @@ public class Register extends AppCompatActivity
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
+                            loginButton.setEnabled(true);
+                            Intent gotoAccount = new Intent(getApplicationContext(), Account.class);
+                            startActivity(gotoAccount);
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(Register.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            loginButton.setEnabled(true);
                             //updateUI(null);
                         }
 

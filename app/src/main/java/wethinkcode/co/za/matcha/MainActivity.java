9bcd89070195a75f19dcb947db84ce582631c2a6
Rheeders <sparky79131>
2019-07-03 12:09:25 +0200

    cleanedUp

--- app/src/main/java/wethinkcode/co/za/matcha/MainActivity.java
+++ app/src/main/java/wethinkcode/co/za/matcha/MainActivity.java
@@ -24,11 +24,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -40,9 +38,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import org.json.simple.parser.JSONParser;
-
 
 public class MainActivity extends AppCompatActivity {
 
